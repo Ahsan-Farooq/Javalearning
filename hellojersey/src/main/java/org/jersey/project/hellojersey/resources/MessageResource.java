07256bc873851598fb547ef6769a21e817030dd0
Ahsan-Farooq <ahsanfarooq4@gmail.com>
2020-02-21 12:27:08 +0500

    CRUD Operation

--- hellojersey/src/main/java/org/jersey/project/hellojersey/resources/MessageResource.java
+++ hellojersey/src/main/java/org/jersey/project/hellojersey/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.jersey.project.hellojersey.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,23 +16,36 @@ import org.jersey.project.hellojersey.model.Message;
 import org.jersey.project.hellojersey.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	MessageService messageservice =new MessageService();
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages()
 	{
 		return messageservice.getAllMessages();
 	}
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId")Long Id,Message message)
+	{
+		message.setId(Id);
+		return messageservice.updateMessage(message);
+		
+	}
+	@DELETE
+	@Path("/{messageId}")
+	public Message deleteMessage(@PathParam("messageId")Long Id )
+	{
+		return messageservice.removeMessage(Id);
+		
+	}
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message addMessage(Message messaged)
+	public Message addMessage(@PathParam("messageId")Message messaged)
 	{
 		return messageservice.addMessage(messaged);
 	}
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{messageId}")
 	public Message getMessage(@PathParam("messageId")Long Id)
 	{
